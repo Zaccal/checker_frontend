b9e2b8a9e26d71c7264efc736187a939885b7755
Adlet <ksss90411@gmail.com>
2025-07-06 01:44:38 +0500

    feat: refactor login form ✨

---
+++ components/shared/AuthForms/LoginFormHeader.tsx
@@ -0,0 +1,12 @@
+const LoginFormHeader = () => {
+	return (
+		<>
+			<h1 className="text-center font-bold text-2xl">Welcome back! ✌️</h1>
+			<p className="text-center text-muted-foreground mt-2">
+				Enter your email and password to log in to your account.
+			</p>
+		</>
+	)
+}
+
+export default LoginFormHeader
---
+++ hooks/use-login-submit.ts
@@ -0,0 +1,67 @@
+import { signIn } from '@/lib/auth'
+import { VERIFICATION_AUTH_CODE } from '@/lib/constants/constants'
+import { isEmail } from '@/lib/isEmail'
+import { TypeLoginSchema } from '@/lib/schemas/logIn.schema'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { toast } from 'sonner'
+
+export const useLoginSubmit = () => {
+	const router = useRouter()
+	const searchParams = useSearchParams()
+	const callbackUrl = searchParams.get('callbackUrl') || '/dashboard'
+
+	const handleSubmit = async ({
+		emailOrUsername,
+		password,
+	}: TypeLoginSchema) => {
+		if (!isEmail(emailOrUsername) && password) {
+			await signIn.username(
+				{
+					username: emailOrUsername,
+					password,
+				},
+				{
+					onSuccess() {
+						router.push(callbackUrl)
+						toast.success('Welcome back!')
+					},
+					onError: ({ error }) => {
+						if (error.code === VERIFICATION_AUTH_CODE) {
+							toast.info('Email not verified', {
+								description: 'You have to use a email for log in.',
+							})
+						} else {
+							toast.error('Opss, somthing wrong, try again', {
+								description: error.error.message || 'Somthing is wrong',
+							})
+						}
+					},
+				}
+			)
+		} else {
+			await signIn.magicLink(
+				{
+					email: emailOrUsername,
+					callbackURL: `${window.location.origin}${callbackUrl}`,
+				},
+				{
+					onSuccess: () => {
+						toast.success('Magic link sent', {
+							description: 'Check your email for a login link.',
+						})
+					},
+					onError: ({ error }) => {
+						toast.error('Failed to send magic link', {
+							description:
+								error.message || 'There was a problem sending the magic link',
+						})
+					},
+				}
+			)
+		}
+	}
+
+	return {
+		handleSubmit,
+	}
+}
