5df5ecb4db72bffe4637cdf1afb7c5d06df0270e
Adlet <ksss90411@gmail.com>
2025-07-06 01:35:09 +0500

    feat: add localStorage persistence to OTP timer with custom hooks & refactor ✨

--- app/(auth)/auth/otp-code/verify/page.tsx
+++ app/(auth)/auth/otp-code/verify/page.tsx
@@ -1,118 +1,26 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
-import {
-	Form,
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from '@/components/ui/form'
-import {
-	InputOTP,
-	InputOTPGroup,
-	InputOTPSeparator,
-	InputOTPSlot,
-} from '@/components/ui/input-otp'
-import { signIn } from '@/lib/auth'
-import {
-	otpCodeFormSchema,
-	otpCodeFormSchemaType,
-} from '@/lib/schemas/otpCodeForm.schema'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { useForm } from 'react-hook-form'
-import { toast } from 'sonner'
+import { useOtpTimer } from '@/hooks/use-otp-timer'
+import OtpForm from '@/components/shared/AuthForms/OtpForm'
+import { useOtpVerifySubmit } from '@/hooks/use-otp-verify-submit'
 
 const page = () => {
-	const form = useForm({
-		resolver: zodResolver(otpCodeFormSchema),
-		defaultValues: {
-			code: '',
+	const { isRunTimer, timeLeft, startTimer, resetTimer } = useOtpTimer(120)
+	const { handleSubmit } = useOtpVerifySubmit({
+		callback: startTimer,
+		onSuccess: () => {
+			resetTimer()
 		},
 	})
-	const searchParams = useSearchParams()
-	const email = searchParams.get('email')
-	const router = useRouter()
-
-	const handleSubmit = async ({ code }: otpCodeFormSchemaType) => {
-		if (email) {
-			await signIn.emailOtp(
-				{
-					email,
-					otp: code,
-				},
-				{
-					onSuccess: () => {
-						router.push('/dashboard')
-						toast.success('Welcome!', {
-							description:
-								'You have successfully signed in with your OTP code.',
-						})
-					},
-					onError: error => {
-						toast.error('Something went wrong', {
-							description:
-								error instanceof Error
-									? error.message
-									: 'An unexpected error occurred while verifying your OTP code. Please try again.',
-						})
-					},
-				}
-			)
-		} else {
-			toast.error(
-				'Email is missing from the URL. Please try again or request a new OTP link.'
-			)
-			router.back()
-		}
-	}
-
-	const { isSubmitting } = form.formState
 
 	return (
 		<div className="w-full h-screen flex items-center justify-center">
 			<div className="">
-				<Form {...form}>
-					<form
-						className="space-y-5"
-						onSubmit={form.handleSubmit(handleSubmit)}
-					>
-						<FormField
-							control={form.control}
-							name="code"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>One-Time Password</FormLabel>
-									<FormMessage />
-									<FormControl>
-										<InputOTP disabled={isSubmitting} maxLength={6} {...field}>
-											<InputOTPGroup>
-												<InputOTPSlot index={0} />
-												<InputOTPSlot index={1} />
-												<InputOTPSlot index={2} />
-											</InputOTPGroup>
-											<InputOTPSeparator />
-											<InputOTPGroup>
-												<InputOTPSlot index={3} />
-												<InputOTPSlot index={4} />
-												<InputOTPSlot index={5} />
-											</InputOTPGroup>
-										</InputOTP>
-									</FormControl>
-									<FormDescription>
-										Please enter the one-time password sent to your email.
-									</FormDescription>
-								</FormItem>
-							)}
-						/>
-						<Button disabled={isSubmitting} type="submit">
-							Submit
-						</Button>
-					</form>
-				</Form>
+				<OtpForm
+					onSubmit={handleSubmit}
+					isRunTimer={isRunTimer}
+					timeLeft={timeLeft}
+				/>
 			</div>
 		</div>
 	)
