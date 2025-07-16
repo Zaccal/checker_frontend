93bc445ca2f899ec9aabe5d6e7ab538a6d4804dd
Adlet <ksss90411@gmail.com>
2025-07-16 16:03:43 +0500

    feat: fixed middleware and remove manual redirect for betterauth

--- hooks/use-login-submit.ts
+++ hooks/use-login-submit.ts
@@ -36,7 +36,6 @@ export const useLoginSubmit = () => {
 							})
 						}
 					},
-					redirect: 'manual',
 				}
 			)
 		} else {
@@ -57,7 +56,6 @@ export const useLoginSubmit = () => {
 								error.message || 'There was a problem sending the magic link',
 						})
 					},
-					redirect: 'manual',
 				}
 			)
 		}
