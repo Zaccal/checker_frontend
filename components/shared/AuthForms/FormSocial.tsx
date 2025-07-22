67997046af93dd991a2cd5b66cdcb5eb7e18e60b
Adlet <ksss90411@gmail.com>
2025-07-22 15:14:13 +0500

    feat: added social auth by github ✨

--- components/shared/AuthForms/FormSocial.tsx
+++ components/shared/AuthForms/FormSocial.tsx
@@ -1,18 +1,48 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
 import Github from '../Common/Github'
 import FormFooter from './ui/FormFooter'
+import { signIn } from '@/lib/auth'
+import { toast } from 'sonner'
+import { useState } from 'react'
 
 const FormSocial = () => {
-	return (
-		<>
-			<form>
-				<Button variant={'outline'} className="w-full font-bold ">
-					<Github size={28} /> Github
-				</Button>
+	const [isLoading, setIsLoading] = useState(false)
 
-				<FormFooter />
-			</form>
-		</>
+	const handleGithubSign = async () => {
+		setIsLoading(true)
+		await signIn.social(
+			{
+				provider: 'github',
+				callbackURL: `${window.location.origin}/dashboard`,
+			},
+			{
+				onError: ({ error }) => {
+					toast.error('Github sign in failed', {
+						description: error.message,
+					})
+				},
+				onResponse: () => {
+					setIsLoading(false)
+				},
+			}
+		)
+	}
+
+	return (
+		<div>
+			<Button
+				onClick={handleGithubSign}
+				disabled={isLoading}
+				variant={'outline'}
+				className="w-full font-bold "
+			>
+				<Github size={28} /> Github
+			</Button>
+
+			<FormFooter />
+		</div>
 	)
 }
 
