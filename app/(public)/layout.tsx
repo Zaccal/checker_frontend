ecd6f283f80740f1008aeee67bbaf020bf51646b
Adlet <ksss90411@gmail.com>
2025-07-22 18:47:52 +0500

    feat: added adaptive for auth page

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -4,14 +4,14 @@ import type { ReactNode } from 'react'
 
 const layout = ({ children }: { children: ReactNode }) => {
 	return (
-		<div className="w-full, h-screen grid grid-cols-2">
-			<div className="bg-primary h-full w-full flex flex-col justify-between px-10 py-10">
+		<div className="w-full, h-screen md:grid grid-rows-1 grid-cols-2">
+			<div className="bg-primary md:h-full w-full flex justify-center items-center md:items-start md:flex-col md:justify-between px-10 py-10">
 				<div className="flex items-center gap-2 text-primary-foreground">
 					<CircleCheckBigIcon className="size-8" />
 					<span className="font-bold text-xl">Checker</span>
 				</div>
 
-				<div className="text-primary-foreground  w-full">
+				<div className="text-primary-foreground  w-full hidden md:block">
 					<p className="text-xl font-semibold mb-3">
 						The secret of getting ahead is getting started. Don&apos;t just
 						dream about it, do it. Your future is created by what you do today.
@@ -27,7 +27,7 @@ const layout = ({ children }: { children: ReactNode }) => {
 				</div>
 			</div>
 
-			<div className="px-10 py-10">{children}</div>
+			<div className="px-10 py-10 w-full">{children}</div>
 		</div>
 	)
 }
--- components/shared/AuthForms/ui/FormHeader.tsx
+++ components/shared/AuthForms/ui/FormHeader.tsx
@@ -3,13 +3,19 @@ import Link from 'next/link'
 interface FormHeaderProps {
 	title: string
 	link: string
+	description: string
 }
 
-const FormHeader = ({ link, title }: FormHeaderProps) => {
+const FormHeader = ({ link, title, description }: FormHeaderProps) => {
 	return (
 		<>
-			<div className="w-full flex font-semibold justify-end">
-				<Link href={link}>{title}</Link>
+			<div className="w-full flex font-semibold justify-center mb-7 md:justify-end">
+				<p className="">
+					<span className="text-muted-foreground">{description}</span>{' '}
+					<Link className="text-primary" href={link}>
+						{title}
+					</Link>
+				</p>
 			</div>
 		</>
 	)
