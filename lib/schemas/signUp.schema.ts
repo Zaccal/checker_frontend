283fcb8b030c17e4bbcab62179e2f35ff058ac42
Adlet <ksss90411@gmail.com>
2025-07-22 16:43:08 +0500

    feat: improved password field for sign-up 🎉

--- lib/schemas/signUp.schema.ts
+++ lib/schemas/signUp.schema.ts
@@ -1,17 +1,11 @@
 import { z } from 'zod'
 
-export const signUpSchema = z
-	.object({
-		email: z.string().email(),
-		username: z.string().min(2).max(20).regex(/^\w+$/, {
-			message: 'Username can only contain letters, numbers, and underscores',
-		}),
-		password: z.string().min(8),
-		confirmPassword: z.string().min(8),
-	})
-	.refine(data => data.password === data.confirmPassword, {
-		message: "Passwords don't match",
-		path: ['confirmPassword'],
-	})
+export const signUpSchema = z.object({
+	email: z.string().email(),
+	username: z.string().min(2).max(20).regex(/^\w+$/, {
+		message: 'Username can only contain letters, numbers, and underscores',
+	}),
+	password: z.string().min(8),
+})
 
 export type TypeSingUpSchema = z.infer<typeof signUpSchema>
