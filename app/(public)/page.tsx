3265b9a104c20641c88d31c6d96444a99e4febab
Adlet <ksss90411@gmail.com>
2025-07-23 19:32:12 +0500

    fix: fixed building

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -3,10 +3,12 @@ import FormWrapper from '@/components/shared/AuthForms/ui/FormWrapper'
 import FormSignUp from '@/components/shared/AuthForms/FormSignUp'
 import FormSeparator from '@/components/shared/AuthForms/ui/FormSeparator'
 import FormSocial from '@/components/shared/AuthForms/FormSocial'
+import { Suspense } from 'react'
+import Fallback from '@/components/shared/Common/Fallback'
 
 const page = () => {
 	return (
-		<>
+		<Suspense fallback={<Fallback />}>
 			<FormHeader
 				description="Already have an account?"
 				link="/login"
@@ -16,7 +18,7 @@ const page = () => {
 			<FormWrapper>
 				<FormSignUp /> <FormSeparator /> <FormSocial />
 			</FormWrapper>
-		</>
+		</Suspense>
 	)
 }
 
