ca5820bdf969736c02736fd81c15fef401d29f0f
Adlet <ksss90411@gmail.com>
2025-07-25 20:48:28 +0500

    feat: made constant of public paths & fixed naming 💅

--- lib/constants/constants.ts
+++ lib/constants/constants.ts
@@ -1,34 +1,11 @@
-import { BriefcaseBusiness, Calendar, House, Inbox } from 'lucide-react'
-import type { AppSidebar } from '@/lib/types/components.type'
-
-export const SIDEBAR_ITEMS_DEFAULT: AppSidebar[] = [
-	{
-		title: 'Inbox',
-		icon: Inbox,
-		url: '/inbox',
-		id: 1,
-	},
-	{
-		title: 'Journal',
-		icon: Calendar,
-		url: '/journal',
-		id: 2,
-	},
-]
-
-export const SIDEBAR_ITEMS: AppSidebar[] = [
-	{
-		title: 'Work',
-		icon: BriefcaseBusiness,
-		url: '/work',
-		id: 1,
-	},
-	{
-		title: 'House deals',
-		icon: House,
-		url: '/house',
-		id: 2,
-	},
+export const PUBLIC_PATHS = [
+	'/auth/forgot-password',
+	'/auth/magic-link/verify',
+	'/auth/otp-code/verify',
+	'/auth/reset-password',
+	'/auth/forgot-password/',
+	'/login',
+	'/',
 ]
 
 export const ICON_RENDER_LIMIT = 128
