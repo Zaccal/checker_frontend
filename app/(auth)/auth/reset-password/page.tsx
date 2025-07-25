f661141d09598060af892e736de34edc5e808c89
Adlet <ksss90411@gmail.com>
2025-07-26 00:34:47 +0500

    feat: finished forgot-password funcanality 🎉

---
+++ app/(auth)/auth/reset-password/page.tsx
@@ -0,0 +1,28 @@
+import ResetPasswordFormFields from '@/components/shared/ResetPasswordFormFields'
+import { RectangleEllipsis } from 'lucide-react'
+
+const page = () => {
+	return (
+		<>
+			<div className="w-full h-screen flex items-center justify-center">
+				<div className="max-w-md">
+					<div className="flex flex-col items-center gap-5">
+						<div className="p-2 w-fit border border-border rounded-lg">
+							<RectangleEllipsis />
+						</div>
+						<div className="text-center space-y-3">
+							<h1 className="text-2xl font-bold">Reset your password</h1>
+							<p className="text-muted-foreground">
+								Please enter your new password below. Make sure to remember it
+								for future logins.
+							</p>
+						</div>
+					</div>
+					<ResetPasswordFormFields />
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default page
