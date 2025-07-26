0a3b09166564519de5411d434fb72e985be0c51a
Adlet <ksss90411@gmail.com>
2025-07-27 00:13:11 +0500

    feat: finished funcanality change email for OTP Email sign up ✉️

---
+++ app/api/auth/email-otp/change-email/route.ts
@@ -0,0 +1,22 @@
+export async function POST(req: Request) {
+	const apiUrl = process.env.NEXT_PUBLIC_API_URL
+
+	if (!apiUrl) {
+		return new Response(JSON.stringify({ title: 'API URL is not defined' }), {
+			status: 500,
+			headers: { 'Content-Type': 'application/json' },
+		})
+	}
+
+	const body = (await req.json()) as { newEmail: string; oldEmail: string }
+
+	const response = await fetch(`${apiUrl}/auth-custom/email-otp/change-email`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(body),
+	})
+
+	return response
+}
--- hooks/index.ts
+++ hooks/index.ts
@@ -1,3 +1,4 @@
 export * from './useTimer/useTimer'
 export * from './useDidUpdate/useDidUpdate'
 export * from './useIsomorphicLayoutEffect/useIsomorphicLayoutEffect'
+export * from './useBoolean/useBoolean';
