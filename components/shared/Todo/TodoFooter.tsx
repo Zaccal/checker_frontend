f73949af6c64a0623a900484b87390e3f96aec9e
Adlet <ksss90411@gmail.com>
2025-09-08 12:11:38 +0500

    dev ✨ fix: fixed dot ui display

--- components/shared/Todo/TodoFooter.tsx
+++ components/shared/Todo/TodoFooter.tsx
@@ -5,29 +5,26 @@ import TodoStatus from './TodoStatus'
 import TodoTags from './TodoTags'
 
 export function TodoFooter() {
-  const { subTasks, expiresAt, tags } = todoContext.useSelect(state => state)
-  const isEverySubtaskComplited = subTasks.every(subtask => subtask.completed)
+  const { subTasks, tags } = todoContext.useSelect(state => state)
   const subTasksNotComplitedLength = subTasks.filter(
     data => !data.completed,
   ).length
+  const hasSteps = subTasks.length > 0
+  const hasTags = tags.length > 0
 
   return (
     <>
       <div className="flex flex-col gap-2 mt-3 items-start">
         <div className="flex items-center gap-2">
           <TodoStatus />
-          {subTasks.length > 0 && expiresAt ? (
-            <span className="dot"></span>
-          ) : null}
-          {Boolean(subTasks.length) && (
+          {(hasSteps || hasTags) && <div className="dot" />}
+          {hasSteps && (
             <span className="text-sm text-muted-foreground">
               {subTasksNotComplitedLength} Steps
             </span>
           )}
-          {tags.length && isEverySubtaskComplited ? (
-            <span className="dot"></span>
-          ) : null}
-          <TodoTags />
+          {hasSteps && hasTags && <div className="dot" />}
+          {hasTags && <TodoTags />}
         </div>
       </div>
     </>
